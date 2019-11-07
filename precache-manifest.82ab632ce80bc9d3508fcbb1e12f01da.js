e5cbd8d6b1ce0f54acf00e67fc32148c737c8f93
Kotya Shmotoloha <kostiashmotoloha@gmaol.com>
2019-11-07 22:59:46 +0200

    Updates

---
+++ precache-manifest.82ab632ce80bc9d3508fcbb1e12f01da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51737860f206958a0ebfc65ca166a2c5",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "cafcfc7c4e2b76dde638",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.c0cfabd3.chunk.css"
+  },
+  {
+    "revision": "771772d17923b63bd89e",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.f1444d2e.chunk.js"
+  },
+  {
+    "revision": "cafcfc7c4e2b76dde638",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.29000181.chunk.js"
+  },
+  {
+    "revision": "5d0ef52b57f9d38bad3e",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.9c1c847c.js"
+  }
+]);
